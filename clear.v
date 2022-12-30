5b5706b677812a387081f75f8e85c53392898c8d
lisanieh <an4106018@gs.ncku.edu.tw>
2022-12-30 14:45:31 +0800

    change game rule, reset button means start to answer

--- clear.v
+++ clear.v
@@ -1,11 +1,18 @@
-module clear(state,match,in,keypadBuf);
+module clear(state,seven_reset,in,keypadBuf);
 
-input state,match;
+input seven_reset,state;
 input [3:0] in;
 output reg[3:0] keypadBuf;	
 
-always@(posedge state or posedge match)
+always@(seven_reset or in)
 begin
-	keypadBuf <= 4'h0;
+	if(seven_reset != state)
+	begin
+		keypadBuf <= 4'h0;
+	end
+	else
+	begin
+		keypadBuf <= in;
+	end
 end
 endmodule		
